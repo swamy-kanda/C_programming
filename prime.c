11c1339e71c0480103ebd4896e0ead8922df23bf
Swamy Kanda <swamysaikanda1236@gmail.com>
2026-03-16 14:47:10 +0530

    new branch is created

---
+++ prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int is_prime(int n)
+{
+	if(n<2)
+		return 0;
+
+	for(int i=2;i*i<=n;i++)
+	{
+		if(n%i == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int min = 10,max = 200;
+	printf("primes = ");
+	for(int i = min;i<=max;i++)
+	{
+		if(is_prime(i))
+		{
+			printf("%d ",i);
+		}
+	}
+	printf("\n");
+
+}
